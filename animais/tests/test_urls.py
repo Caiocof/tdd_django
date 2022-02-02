7dd62c55070659055a4e2138fad02fcffa04d950
unknown <caiooliveira3652@outlook.com>
2022-02-02 06:31:22 -0300

    inicia o template de animais e cria o test da rota index

--- animais/tests/test_urls.py
+++ animais/tests/test_urls.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.test import RequestFactory
+from django.urls import reverse
+
+from animais.views import index
 
 
+class AnimaisUrlsTestCase(TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_url_index(self):
+        """Teste se a home utiliza a função index da view"""
+        request = self.factory.get('/')
+        with self.assertTemplateUsed('index.html'):
+            response = index(request)
+            self.assertEqual(response.status_code, 200)
--- setup/tests.py
+++ setup/tests.py
@@ -1,4 +1,6 @@
 from django.test import LiveServerTestCase
+
+import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -6,14 +8,14 @@ from selenium.webdriver.common.by import By
 class AnimaisTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome('E:\Projetos\TEST-AUTOMATIZADO\\tdd_django\chromedriver')
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
 
     def test_busca_animal(self):
         """Test para verificar a usabilidade de buscar animais"""
-        driver = self.browser.get(self.live_server_url + '/')
+        self.browser.get(self.live_server_url + '/')
 
         # Busca por "Busca Animal" no menu
         element = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
